0fd50a673b18a86016287783badd6b6190bd8286
anthony.asadu <anthony.asadu@DEVAPP45>
2021-02-11 00:15:31 +0100

    Added Appropriate Table for Slips

---
+++ code.py
@@ -0,0 +1,10 @@
+firstName = input("Enter First Name : ")
+lastName = input("Enter Last Name : ")
+
+space = " "
+intro = "Your Name is "
+
+fullName = intro + firstName + space + lastName
+
+
+print(fullName)
---
+++ learn.py
@@ -0,0 +1,12 @@
+
+
+'''
+Data : 
+    - Retention
+        * Box : Data are represented as variables in code
+    - Retrieve
+        
+Manipulation : 
+
+Presentation :
+'''
